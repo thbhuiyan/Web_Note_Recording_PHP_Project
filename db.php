bba712e80b835f5d9a6683dd88c83c89b8b06ac8
thbhuiyan <th.bhuiyan124578@gmail.com>
2022-09-23 01:10:13 +0600

    database connected form created as well as page designed

--- db.php
+++ db.php
@@ -1,1 +1,16 @@
-#jlfja
+<?php
+mysqli_report(MYSQLI_REPORT_OFF);
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "notes";
+$con = mysqli_connect($server,$username,$password,$database);
+
+if(!$con){
+    die("The connection has failed!\n");
+}
+else{
+    echo "Connected with the '$database' database successfully\n";
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <!-- Bismillahir Rahmanir Rahim -->
+<?php 
+require_once('db.php');
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,51 +12,74 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>CRUD APP!</title>
+    <title>iNotes - Notes taking make easy!</title>
   </head>
   <body>
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">iNotes Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Contact US</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      </nav>
+      <div class="container my-5">
+        <h2>Add a NOTE</h2>
+        <form>
+            <div class="form-group">
+              <label for="title">Note Title: </label>
+              <input type="text" class="form-control" id="title" aria-describedby="title" name="description">
+            </div>
 
-    <h1>
-      
-    </h1>
+            <div class="form-group">
+                <label for="description">Description: </label>
+                <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+              </div>
+              <small id="title" class="form-text text-muted">We'll never share your records with anyone else.</small>
+            <button type="submit" class="btn btn-primary">Add Note</button>
+        </form>
+      </div>
+
+      <div class="container">
+        <?php
+        $sql= "SELECT * FROM `tnotes`";
+        $fireq = mysqli_query($con,$sql);
+///////////REcord insert the following one and start from 21 minutes INSERT INTO `tnotes` (`sno`, `title`, `description`, `time`) VALUES ('1.', 'shop for Today', 'Buy the followings:\r\n1. Fish\r\n2. Chicken\r\n3. Spice\r\n4. Rice\r\n5. Eggs\r\netc', '2022-09-22 21:06:58.000000');
 
 
+
+
+        while($row = mysqli_fetch_assoc($fireq)){
+          echo $row['sno'].". Title is: ".$row['title']. " and description is:<br>".$row['description'];
+        }
+        ?>
+      </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
